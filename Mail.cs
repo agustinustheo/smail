9e5e016b701a3b0cdd590bbafd00785e057147fb
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-07-16 01:15:46 +0700

    init.

---
+++ Mail.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace SimpleMailCLI
+{
+    public static class Mail
+    {
+        public static void Send(List<string> recipients, string subject, string displayName, string body, bool enableSsl = true)
+        {
+            try
+            {
+                // Get configuration values
+                var appConfig = new AppConfiguration();
+                int _port = appConfig.MailPort;
+                string _host = appConfig.MailHost;
+                string _sender = appConfig.MailUser;
+                string _password = appConfig.MailPassword;
+
+                // Initialize the SMTP host.
+                SmtpClient client = new SmtpClient(_host);
+                client.Port = _port;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(_sender, _password);
+                client.EnableSsl = enableSsl;
+                client.Credentials = credentials;
+
+                // Specify the message content.
+                MailMessage message = new MailMessage();
+                foreach (string recipient in recipients)
+                {
+                    message.To.Add(new MailAddress(recipient));
+                }
+
+                // Specify the email sender.
+                // Create a mailing address that includes a UTF8 character
+                // in the display name.
+                message.From = new MailAddress(_sender, displayName, System.Text.Encoding.UTF8);
+
+                // Include some non-ASCII characters in body and subject.
+                message.Subject = subject;
+                message.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // Make the email body
+                message.Body = body;
+                message.BodyEncoding = System.Text.Encoding.UTF8;
+
+                // Send email
+                client.Send(message);
+
+                // Clean up.
+                message.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleMailCLI
+{
+    class Program
+    {
+        private static List<string> commands = new List<string>
+        {
+            "-r",
+            "-s",
+            "-d",
+            "-c"
+        };
+        static void Main(string[] args)
+        {
+            string Recipient = "";
+            string Subject = "";
+            string DisplayName = "";
+            string Content = "";
+
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine($"Arguments cannot be empty");
+                }
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith('/') || args[i].StartsWith('-'))
+                    {
+                        if (!commands.Contains(args[i]))
+                        {
+                            throw new Exception($"Invalid flag {args[i]}");
+                        }
+                        if (i + 1 >= args.Length)
+                        {
+                            throw new Exception($"Invalid entry for flag {args[i]}");
+                        }
+                        if (args[i + 1].StartsWith('/') || args[i + 1].StartsWith('-'))
+                        {
+                            throw new Exception($"Invalid entry for flag {args[i]}");
+                        }
+
+                        switch (args[i])
+                        {
+                            case "-r":
+                                Recipient = args[i + 1];
+                                break;
+                            case "-s":
+                                Subject = args[i + 1];
+                                break;
+                            case "-d":
+                                DisplayName = args[i + 1];
+                                break;
+                            case "-c":
+                                Content = args[i + 1];
+                                break;
+                        }
+                    }
+                }
+
+                if (!(string.IsNullOrEmpty(Recipient) || string.IsNullOrEmpty(Subject) ||
+                    string.IsNullOrEmpty(DisplayName) || string.IsNullOrEmpty(Content)))
+                {
+                    Mail.Send(new List<string>
+                    {
+                        Recipient
+                    }, Subject, DisplayName, Content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
